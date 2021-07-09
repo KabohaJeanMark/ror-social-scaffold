48fd2c3423900070d25cf9a8785dc84a7f21e4bc
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-07-09 11:04:55 +0300

    Refactor the method to send a friendship request to pick the user_id as the current_user.id and pass it in the friendship params

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,19 +4,18 @@ class FriendshipsController < ApplicationController
   end
 
   def create
-    @friendship = current_user.friendships.build(friendship_params)
-    
+    @friendship = Friendship.new(friendship_params)
+
     if @friendship.save
-      redirect_to current_user, notice: "Friend request sent"
+      redirect_to current_user, notice: 'Friend request sent'
     else
-      render :new,  notice: "Request not sent"
+      render :new, notice: 'Request not sent'
     end
   end
 
   private
 
   def friendship_params
-    params.require(:friendship).permit(:friend_id, :confirmed) # user_id
+    params.require(:friendship).permit(:user_id, :friend_id, :confirmed)
   end
-  
 end
