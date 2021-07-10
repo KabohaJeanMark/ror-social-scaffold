dbf3a091e887f2d2e25e7d0df9d61c7badba35b5
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-07-10 18:23:35 +0300

    Add validation for the Friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,8 @@
 class Friendship < ApplicationRecord
+  validates :user_id, presence: true
+  validates :friend_id, presence: true
+  validates :confirmed, presence: true
+
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 end
