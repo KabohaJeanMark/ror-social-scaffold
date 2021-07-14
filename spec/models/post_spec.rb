f80b232e87561595a36f93fe29e4bcfea065ee0f
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-07-14 11:57:16 +0300

    Add 2 more tests to posts model using proper subjects to provide more behaviour driven tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  let(:user1) {User.create(name: 'jean', email: 'jean@gmail.com', password: '123456')}
+  let(:post1) {Post.create(content: 'First post', user_id: user1.id)}
+  let(:post2) {Post.create(content: '', user_id: user1.id)}
   context 'validation test' do
     it 'is valid with valid attributes' do
       expect(Post.new).to_not be_valid
@@ -9,6 +12,14 @@ RSpec.describe Post, type: :model do
     it 'is not valid without content' do
       post = Post.new(content: nil)
       expect(post).to_not be_valid
+    end
+
+    it 'is valid with proper content filled in' do
+      expect(post1).to be_valid
+    end
+
+    it 'is not valid if the post content is blank' do
+      expect(post2).to_not be_valid
     end
   end
 end
