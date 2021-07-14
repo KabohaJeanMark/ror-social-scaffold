10612b13e3a128e03e84294bf0b8a0b55ade2568
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-07-14 12:19:36 +0300

    Add more tests for the like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
+  let(:user1) {User.create(name: 'jean', email: 'jean@gmail.com', password: '123456')}
+  let(:post1) {Post.create(content: 'First post', user_id: user1.id)}
+
   it 'is not valid without valid attributes' do
     expect(Like.new).to_not be_valid
+  end
+
+  it 'is valid with proper attributes' do
+    expect(Like.new(user_id: user1.id, post_id: post1.id ))
   end
 end
