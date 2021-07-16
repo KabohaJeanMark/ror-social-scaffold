a29b68f46a5416d1777820ea55d7f16d430f3828
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-07-16 12:41:03 +0300

    Add association to get friends with a through table of accepted friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,15 +13,10 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
+  has_many :accepted_friendships, -> { where confirmed: true }, class_name: 'Friendship'
+  has_many :friends, through: :accepted_friendships
+
   def friend?(user)
     friends.include?(user)
-  end
-
-  private
-
-  def friends
-    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-    friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
-    friends_array.compact
   end
 end
